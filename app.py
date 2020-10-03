809e2068a905a26dc25135774c705a7998acd87c
aravk33 <aravkasi@gmail.com>
2020-10-03 08:32:33 +0530

    Render basic star map

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return send_from_directory("templates", "index.html")
+
+
+@app.route("/scripts/<path:path>")
+def send_script(path):
+    return send_from_directory("static/scripts", path)
+
+
+@app.route("/styles/<path:path>")
+def send_styles(path):
+    return send_from_directory("static/styles", path)
+
+
+@app.route("/images/<path:path>")
+def send_images(path):
+    return send_from_directory("static/images", path)
